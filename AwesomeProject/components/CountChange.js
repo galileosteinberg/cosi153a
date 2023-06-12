769be22d81b6b666f6522b6e2351d0d7e608adba
galileosteinberg <gsteinberg@brandeis.edu>
2023-06-12 12:01:05 -0400

    refactored working countchange app.

--- AwesomeProject/components/CountChange.js
+++ AwesomeProject/components/CountChange.js
@@ -1,16 +1,8 @@
 import React,{useState} from 'react';
 import {View, Button, Text} from 'react-native';
 
-const CoinCount = ({coinName, coinValue}) => {
-    return (
-        <View style = {{flex: 1, flexDirection: 'row', }}>
-                <Button
-                    title = {coinName}
-                    onPress = {() => setChange(change+coinValue)}
-                    />
-        </View>
-    )
-}
+import CoinCount from './CoinCount';
+
 const App = () => {
     const [change,setChange] = useState(0)
 
@@ -23,27 +15,12 @@ const App = () => {
                 {change}
             </Text>
             <View style={{flex: 1, flexDirection: 'row'}} >
-                <CoinCoint coinName = "Penny" coinValue = {1} />
-                <Button
-                    title="Pennies"
-                    onPress = {() => setChange(change+1)}
-                />
-                <Button
-                    title="Nickels"
-                    onPress = {() => setChange(change+5)}
-                />
-                <Button
-                    title="Dimes"
-                    onPress = {() => setChange(change+10)}
-                />
-                <Button
-                    title="Quarters"
-                    onPress = {() => setChange(change+25)}
-                />
-                <Button
-                    title="Half Dollars"
-                    onPress = {() => setChange(change+50)}
-                />
+                <CoinCount coinName="Penny" coinValue={1} />
+                <CoinCount coinName="Nickel" coinValue={5} />
+                <CoinCount coinName="Dime" coinValue={10} />
+                <CoinCount coinName="Quarter" coinValue={25} />
+                <CoinCount coinName="Half Dollar" coinValue={50} />
+                
             </View>
       </View>
     )
